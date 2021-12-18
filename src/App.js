8cb7c75806a4c084ff9526a6a40426a6e92b355c
Nk3iru <70352546+Nk3iru@users.noreply.github.com>
2021-12-18 12:55:10 -0600

    changes to package.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-//import pickCollege from './professionalsForm/selectedStudents';
-=======
+
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {withAuthenticator} from '@aws-amplify/ui-react';
@@ -11,31 +9,10 @@ import awsExports from './aws-exports';
 import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 Amplify.configure(awsExports)
->>>>>>> 882942c28ec6e80723a8581589a3c9a3918ba3ee
+
 
 function App(  {signOut, user } ) {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LetsMnt
-        </a>
-      </header>
-   
-    </div>
-
-  
-=======
     <>
        <Navbar bg="light" variant="light" expand="lg" sticky="top">
           <Navbar.Brand href="#home">Letsmtn</Navbar.Brand>
@@ -57,9 +34,8 @@ function App(  {signOut, user } ) {
       <br/>
       <h2>Home Page</h2>
    
-
     </>
->>>>>>> 882942c28ec6e80723a8581589a3c9a3918ba3ee
+
   );
 }
 

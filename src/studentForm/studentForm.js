3d280303f74baefaffa65d638029b47214604a7f
Nk3iru <70352546+Nk3iru@users.noreply.github.com>
2021-12-18 12:22:31 -0600

    pushing studentForm

---
+++ src/studentForm/studentForm.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import ReactDOM from "react-dom";
+import './studentForm.css';
+
+function StudentForm(){
+
+    const [name, setName] = useState("");
+    const [school, setSchool] = useState("");
+    const [major, setMajor] = useState("");   
+
+    render()
+    {
+        render()
+    {
+        return(
+        <form>
+            <label>Name:
+             <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+             </label>
+      
+             <label>School:
+             <input type="text" value={school} onChange={(e) => setSchool(e.target.value)}/>
+             </label>
+        
+      
+            <label>Major:
+             <input type="text" value={major} onChange={(e) => setMajor(e.target.value)}/>
+             </label>
+        
+        </form>
+        );
+    }
+}
+}
+    
+ReactDOM.render(<StudentForm/>, document.getElementById('root'));

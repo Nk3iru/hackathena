882942c28ec6e80723a8581589a3c9a3918ba3ee
Jingheng Li <justsunli@Jinghengs-MacBook-Pro.local>
2021-12-18 10:15:27 -0800

    home page interface

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import awsconfig from './aws-exports';
 import {withAuthenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
-
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 Amplify.configure(awsExports)
 
 function App(  {signOut, user } ) {
   return (
     <>
-        
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+       <Navbar bg="light" variant="light" expand="lg" sticky="top">
+          <Navbar.Brand href="#home">Letsmtn</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/how-we-work">How We Work</Nav.Link>
+                <Nav.Link href="/our-team">Our Team</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              </NavDropdown>
+              </Nav>
+              
+              <Button variant="outline-success" onClick={signOut}>Sign Out</Button>
+            
+          </Navbar.Collapse>
+        </Navbar>
+      <br/>
+      <h2>Home Page</h2>
+   
 
     </>
   );
